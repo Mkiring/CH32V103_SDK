c0fd3f1040a68aaaaf0d847345dc02fd19e410c4
Mkiring <2648876790@qq.com>
2023-05-10 17:04:37 +0800

    update

---
+++ Demo/obj/Peripheral/src/ch32v10x_exti.d
@@ -0,0 +1,85 @@
+Peripheral/src/ch32v10x_exti.o: ../Peripheral/src/ch32v10x_exti.c \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_exti.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Core/core_riscv.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\User/system_ch32v10x.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\User/ch32v10x_conf.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_adc.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_bkp.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_crc.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_dbgmcu.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_dma.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_flash.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_gpio.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_i2c.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_iwdg.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_pwr.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_rcc.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_rtc.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_spi.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_tim.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usart.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_wwdg.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb_host.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\User/ch32v10x_it.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Debug/debug.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x.h \
+ C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_misc.h
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_exti.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Core/core_riscv.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\User/system_ch32v10x.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\User/ch32v10x_conf.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_adc.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_bkp.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_crc.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_dbgmcu.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_dma.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_flash.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_gpio.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_i2c.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_iwdg.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_pwr.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_rcc.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_rtc.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_spi.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_tim.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usart.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_wwdg.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb_host.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_usb.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\User/ch32v10x_it.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Debug/debug.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x.h:
+
+C:\MRS_DATA\workspace\CH32V103C8T6\Peripheral\inc/ch32v10x_misc.h:
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CH32V103_SDK
-    代码来自CH32V103EVT.zip
+    代码来自CH32V103EVT.zip和MRS。
+## Demo 
+    MRS自动生成的空项目。
